3c4b03b97482672abfa51a7117f051c0bb36344d
x <x>
2024-06-12 06:28:36 -0500

    feat: :rocket: Creación de tablas
    Ejercicio del día 2, creación de tablas.

---
+++ Dia_2/Ejercicio_Dia2.sql
@@ -0,0 +1,83 @@
+-- #####################
+-- ### DIA # 2 - Comandos Generales ###
+-- #####################
+
+-- Comando general para revisión de bases de datos creadas
+show databases;
+
+-- Crear base de datos
+
+create database dia2;
+
+-- Utilizar BBDD dia2
+
+use dia2;
+
+-- Crear tabla departamento
+create table departamento (
+    id int auto_increment primary key,
+    nombre varchar(50) not null
+);
+
+-- Crear tabla persona
+create table persona(
+    id int auto_increment primary key,
+    nif varchar(9),
+    nombre varchar(25) not null,
+    apellido1 varchar(50) not null,
+    apellido2 varchar (50),
+    ciudad varchar(25) not null,
+    direccion varchar(50) not null,
+    telefono varchar(9),
+    fecha_nacimiento DATE not null,
+    sexo enum('H','M') not null,
+    tipo enum('profesor','alumno') not null
+);
+
+-- Crear la tabla de profesor
+create table profesor(
+    id_profesor int primary key,
+    id_departamento int not null,
+    foreign key (id_profesor) references persona(id),
+    foreign key (id_departamento) references departamento(id)
+);
+
+-- Crear la tabla de grado
+create table grado(
+	id int auto_increment primary key,
+    nombre varchar(100) not null
+);
+
+-- Crear la tabla de asignatura
+create table asignatura(
+	id int auto_increment primary key,
+    nombre varchar(100) not null,
+    creditos float not null,
+    tipo enum('...') not null,
+    curso tinyint(3) not null,
+    cuatrimestre tinyint(3) not null,
+    id_profesor int(10),
+    id_grado int(10),
+	foreign key (id_profesor) references persona(id),
+	foreign key (id_grado) references grado(id)
+);
+
+
+-- Crear tabla de curso escolar
+create table curso_escolar(
+	id int auto_increment primary key,
+    anyo_inicio year(4) not null,
+    anyo_final year(4) not null
+);
+
+-- Crear tabla de alumno_se_matricula_asignatura
+create table alumno_se_matricula_asignatura(
+	id_alumno int(10),
+    id_asignatura int(10),
+    id_curso_escolar int(10),
+	foreign key (id_alumno) references persona(id),
+    foreign key (id_asignatura) references asignatura(id),
+    foreign key (id_curso_escolar) references curso_escolar(id)
+);
+
+show tables;
